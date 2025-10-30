2699a183209fd3188509d2725618459d1f147ee5
Diego Aranda <arandadiegoa@gmail.com>
2025-10-30 20:47:09 -0300

    Finalización de registro con validaciones

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,194 @@
+"use client";
+import { z } from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z.string().min(3, {
+    message: "El nombre debe tener al menos 3 caracteres.",
+  }),
+  lastName: z.string().min(3, {
+    message: "El apellido debe tener al menos 3 caracteres.",
+  }),
+  email: z.email({
+    message: "Por favor ingresa un email válido",
+  }),
+  password: z.string().min(6, {
+    message: "La contraseña debe tener al menos 6 caracteres",
+  }),
+  confirmPassword: z.string().min(6, {
+    message: "La contraseña debe tener al menos 6 caracteres",
+  }),
+  telephone: z
+    .string()
+    .min(10, { message: "Debe tener al menos 10 caracteres" })
+    .regex(/^[0-9]+$/, { message: "El teléfono solo debe contener números" }),
+})
+.refine(
+    (data) => data.password === data.confirmPassword,
+    {
+      message: "Las contraseñas no coinciden",
+      path:["confirmPassword"]
+    }
+)
+
+
 export function Register() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword:"",
+      telephone: "",
+    },
+  });
+
+  type RegisterFormValues = z.infer<typeof formSchema>;
+
+  function onSubmit(values: RegisterFormValues) {
+    console.log("Datos validados", values);
+  }
+
   return (
-     <div style={{padding:"20px"}}>
-      <h1>Registrarse</h1>
+    <div className="flex items-center justify-center min-h-screen mt-4">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl text-center">Registrarse</CardTitle>
+          <CardDescription className="text-center">
+            Completar todos los campos obligatorios
+          </CardDescription>
+        </CardHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="nombre" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Apellido</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="apellido" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="tu@email.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Contraseña</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="contraseña" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+             <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirmar Contraseña</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="confirmar constraseña" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="telephone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Teléfono</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="teléfono" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit" className="w-full">
+                Enviar
+              </Button>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
     </div>
-  )
+  );
 }
